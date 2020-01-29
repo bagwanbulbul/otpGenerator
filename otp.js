985e430c4629bf86b1852448efe8a6816ce1b27b
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-29 12:58:02 +0530

    otp

--- otp.js
+++ otp.js
@@ -5,11 +5,7 @@ var otpGenerator = require("otp-generator")
 app.get("/otp",function(req,res){
     let response = otpGenerator.generate(4,{upperCase:false,specialChars:false,alphabets:false})
     res.json({"massage":"your otp number","otp":response})
-    // response.then((result)=>{
-    //     res.send(result)
-    // }).catch((err)=>{
-    //     res.send(err)
-    // })
+    
 })
 app.listen(4000, () => console.log('server is listening 4000....'));
 
